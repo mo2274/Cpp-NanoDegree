dd59557155d7fe532a6d257b5fbc19c53c704e36
mo2274 <mohamedzaghlol74111@gmail.com>
2020-04-04 22:56:06 +0200

    impelementation of read from a file function

--- Intoduction to Cpp/read_the_board_from_file.c
+++ Intoduction to Cpp/read_the_board_from_file.c
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -10,17 +11,32 @@ using namespace std;
 
 #define FILEPATH ("G:/embedded projects/eclipse workspace/firstc++code/text.txt")
 
+void printBoard(vector<vector<int>> board);
 void ReadBoardFile(string filePath);
+vector<int> ParseLine(string line);
+
+vector<vector<int>> board;
 
 int main()
 {
 	ReadBoardFile(FILEPATH);
+	printBoard(board);
 
     return 0;
 }
 
+void printBoard(vector<vector<int>> board) {
+	for (unsigned int i = 0; i < board.size(); i++) {
+		for (unsigned int j = 0; j < board[i].size(); j++) {
+			cout << board[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
 void ReadBoardFile(string filePath) {
 	string input;
+	vector<int> output;
 	// create an object for the input file
 	ifstream inputFile{filePath, ios::in};
 	// check for errors
@@ -29,8 +45,38 @@ void ReadBoardFile(string filePath) {
 		exit(EXIT_FAILURE);
 	}
 	// read data from the file
-	while (inputFile) {
-		inputFile >> input;
-		cout << input << "\n";
+	while (getline(inputFile, input)) {
+		output = ParseLine(input);
+		board.push_back(output);
 	}
 }
+
+vector<int> ParseLine(string line) {
+	// vector to store the output
+	vector<int> result;
+	// convert the string into stream
+	istringstream stream(line);
+	// parse the stream
+	string temp;
+	while (stream.good()) {
+		getline(stream, temp, ',');
+		result.push_back(stoi(temp));
+	}
+
+	return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
